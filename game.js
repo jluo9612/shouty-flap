f2842073148ae5f376ed199e94cc735d7d2bbd8b
Jiabei L <luojiabei@gmail.com>
2016-06-16 22:57:01 +0000

    Speech recognition for firing bullets

--- game.js
+++ game.js
@@ -5,10 +5,18 @@ var enemy2;
 var enemyImage2;
 var player;
 var playerImage;
+var bullets;
 var isGameOver;
 var score;
 var fin;
 var recognition = new webkitSpeechRecognition();
+
+if(!("webkitSpeechRecognition" in window)) {
+    upgrade();
+} else {
+    recognition.continuous = true;
+    recognition.interimResults = true;
+}
 
 recognition.start();
 
@@ -22,13 +30,16 @@ recognition.onresult = function(event) {
             interim_transcript += event.results[i][0].transcript;
         }
     }
-    
-    console.log(final_transcript);
+    console.log("interim", interim_transcript);
+    console.log("final", final_transcript);
+    if (interim_transcript.indexOf("fire") >= 0) {
+        createBullet();
+    }
     
 }
 
 recognition.onend = function () {
-    console.log("recognition finished");
+    // console.log("recognition finished");
     recognition.start();
 };
 
@@ -43,7 +54,7 @@ function setup() {
     createCanvas(550, 300);
     mic = new p5.AudioIn();
     mic.start();
-    player = createSprite(width/12, height-5, 10, 5);
+    player = createSprite(width/12, height-20, 10, 5);
     player.addImage(playerImage);
     enemy = createSprite(width, height-80, 20, 5);
     enemy.addImage(enemyImage);
@@ -51,21 +62,10 @@ function setup() {
     enemy2.addImage(enemyImage2);
     score = 0;
     isGameOver = false;
+    
+    bullets = new Group();
+    
 }
-
-// function gameOver() {    
-//     if(isGameOver) {
-//         noLoop();
-//         fin = score;
-//         background(1, 11, 92);
-//         textAlign(CENTER);
-//         fill("white");
-//         textSize(20);
-//         text("Your score:" + fin, camera.position.x, camera.position.y-30);
-//         text("GAME OVER!", width/2, height/2);
-//         text("Press enter key or click anywhere to try again", width/2, 3*height/5);
-//     } 
-// }
 
 function mouseClicked() {
     if (isGameOver) {
@@ -98,16 +98,45 @@ function keyPressed() {
     return false; //prevent any default behavior
 }
 
+function createBullet() {
+    var b = createSprite(player.position.x, player.position.y, 5, 5);
+    bullets.add(b);
+}
+
+
 function draw() {
     background(backgroundImage);
+    
     //get the current amplitude value
     var vol = mic.getLevel();
-    // console.log(vol);
     textAlign(CENTER);
     textSize(50);
     text(score, camera.position.x, 50);
-    drawSprites();
     
+    // if (keyIsDown(RIGHT_ARROW)) {
+    //     for (var i = 0; i < 10; i++) {
+    //         var b = createSprite(player.position.x, player.position.y, 5, 5);
+    //         bullets.add(b);
+    //     }
+    // }
+    
+    for (var i = 0; i < bullets.length; i++) {
+        if(bullets[i].position.x > width){
+            bullets[i].remove();
+        } else {
+            bullets[i].position.x += 2;
+        }
+    }
+    
+    
+    // if(keyCode == RIGHT_ARROW) {
+    //     bullet = createSprite(player.position.x, player.position.y, 5, 5);
+    //     bullet.position.x += 2;
+    //     console.log(bullet);
+    // }
+    
+    drawSprites();
+
     score += 1;
     player.position.y += 3;
     
